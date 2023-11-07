658e42845f3cf8ed8cf9568d021b2b44ef1339c7
wwalk <wwalk831@gmail.com>
2023-11-07 14:38:52 -0500

    Finished assignment

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,21 +1,40 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        val myArray = JSONArray()
+//
+//        for (i in 0 <= .. <= myArray.length()) {
+//            val jsonBook = myArray.getJSONObject(i)
+//
+//            Book(
+//                json
+//            )
+//        }
+
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, editText.text)
+                type = "text/plain"
+            }
 
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
     }
 }
